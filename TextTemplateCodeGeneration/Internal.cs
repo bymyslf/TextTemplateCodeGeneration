774ab89efd8448c2abc9f4cdf21e07bae8713a5d
unknown <luisbarbosa@outlook.com>
2018-01-26 11:29:55 +0000

    First commit

---
+++ TextTemplateCodeGeneration/Internal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+//See: https://blog.rsuter.com/use-t4-texttemplatingfilepreprocessor-in-net-standard-or-pcl-libraries/
+namespace TextTemplateCodeGeneration
+{
+    internal static class T4Extensions
+    {
+        public static MethodInfo GetMethod(this Type type, string method, params Type[] parameters)
+        {
+            return type.GetRuntimeMethod(method, parameters);
+        }
+    }
+}
+
+namespace System.CodeDom.Compiler
+{
+    internal class CompilerErrorCollection : List<CompilerError>
+    {
+    }
+
+    internal class CompilerError
+    {
+        public string ErrorText { get; set; }
+
+        public bool IsWarning { get; set; }
+    }
+}
---
+++ TextTemplateCodeGeneration/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TextTemplateCodeGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var template = new DocumentTemplate()
+            {
+                Source = @"
+
+                Foo:     string Prop1, 
+                         int Prop2;
+
+                Bar:     string FirstName,
+                         string LastName,
+                         int Age;
+                "
+            };
+
+            var classes = template.TransformText();
+            Console.WriteLine(classes);
+            Console.ReadLine();
+        }
+    }
+}
